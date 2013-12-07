994f02b97afe359dd1cfb6bb75d127790bfa92b0
CasaTaloyum <casatwy@msn.com>
2013-12-07 09:39:43 +0800

    Initial Commit

---
+++ LineView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LineView
+//
+//  Created by casa on 13-12-7.
+//  Copyright (c) 2013年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LineViewTests/LineViewTests.m
@@ -0,0 +1,34 @@
+//
+//  LineViewTests.m
+//  LineViewTests
+//
+//  Created by casa on 13-12-7.
+//  Copyright (c) 2013年 casa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LineViewTests : XCTestCase
+
+@end
+
+@implementation LineViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
